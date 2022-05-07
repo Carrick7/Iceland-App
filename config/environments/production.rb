5265c465c9f1b35f43d9303e2c2314d9ccff8a63
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-05-07 01:12:00 +0100

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,11 +28,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "https://explore-iceland.herokuapp.com/"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
