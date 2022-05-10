15d5f6b47a873498454115dc7c44523c678e7a2f
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-05-10 01:23:52 +0100

    added location post method and user names

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5, maximum: 50}
+  validates :title, presence: true, length: {minimum: 1, maximum: 10}
   validates :body, presence: true, length: {minimum: 5, maximum: 1000}
   mount_uploader :image, LocationPhotoUploader
+  belongs_to :user
 end
