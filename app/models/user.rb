b77f69cabdf9e2c7cfceae8ede1cd20f2b0079b2
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-05-14 21:28:12 +0100

    styling done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
    
    has_many :locations, dependent: :destroy
    has_many :comments, dependent: :destroy
-   has_many :likes
+   has_many :likes, dependent: :destroy
 end
